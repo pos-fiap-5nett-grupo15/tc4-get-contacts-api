dd965b00409cf0c6233d172883ef32294ef5fd4a
Caio Maia <caiomaia3@gmail.com>
2024-12-17 16:45:38 -0300

    tests

---
+++ GetContact/UnitTests/Controllers/ContactsTests.cs
@@ -0,0 +1,47 @@
+﻿
+using CreateContact.Infrastructure.Services.Contact;
+using FluentValidation;
+using FluentValidation.Results;
+using GetContact.Api.Controllers.GetContacts;
+using GetContacts.Application.DTOs.Contacts.GetContacts;
+using GetContacts.Application.Handlers.Contacts.GetContacts;
+using MediatR;
+using Moq;
+using TechChallenge3.Domain.Entities.Contact;
+
+namespace TestesUnitarios.Controllers
+{
+    public class ContactsTests
+    {
+        private readonly GetContactByIdHandler _target;
+        private readonly Mock<IValidator<GetContactsByIdRequest>> _validator;
+        private readonly Mock<IContactService> _contactService;
+
+
+
+
+        public ContactsTests()
+        {
+            this._validator = new Mock<IValidator<GetContactsByIdRequest>>();
+            this._contactService = new Mock<IContactService>();
+
+
+
+            this._target = new GetContactByIdHandler(this._validator.Object, this._contactService.Object);
+
+        }
+
+
+        [Fact]
+        public async void GetByIdAsync()
+        {
+
+            this._validator.Setup(x => x.Validate(It.IsAny<GetContactsByIdRequest>())).Returns(new ValidationResult());
+            this._contactService.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(new ContactEntity());
+
+            var result = this._target.Handle(new GetContactsByIdRequest(1), CancellationToken.None);
+
+            Assert.NotNull(result);
+        }
+    }
+}
